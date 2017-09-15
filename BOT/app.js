7494dbe1fa97521467fd7f241791f6c5b25a094b
Aurerianusu <aurelien.beranger@gmail.com>
2017-09-15 21:48:39 +0200

    ajout message nouveau utilisateur
    nouveaux venus

--- BOT/app.js
+++ BOT/app.js
@@ -1,27 +1,49 @@
 var restify = require('restify');
 var botbuilder = require('botbuilder');
 
-//we setup restify
+
+// setup restify server
 
 var server = restify.createServer();
-	server.listen(process.env.port || process.env.PORT || 3987, function(){
-	console.log('%s bot started at %s', server.name, server.url);
+server.listen(process.env.port || process.env.PORT || 39887, function(){
+    console.log('%s bot started at %s', server.name, server.url);
 });
 
-//Chat connector
+// create chat connector
 var connector = new botbuilder.ChatConnector({
-	appId: process.env.APP_ID,
-	appPassword: process.env.APP_SECRET
-})
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_SECRET
+});
 
-
-//Listenning
+// listening for user inputs
 server.post('/api/messages', connector.listen());
 
-// Create bot and default message handler
-var bot = new botbuilder.UniversalBot(connector, function (session) {
-    session.sendTyping();
-    setTimeout(function () {
-        session.send("tu écris lentement...");
-    }, 10000);
+var bot = new botbuilder.UniversalBot(connector, function(session){
+    session.send('tu as dis " %s " Le nombre de caractères de cette phrase est de %s ', session.message.text, session.message.text.length);
+    bot.on('typing', function () {
+        session.send('Tu es en train de chercher tes mots ?');
+    });
+
+   bot.on('conversationUpdate', function(message) {
+
+   if (message.membersAdded && message.membersAdded.length > 0) {
+
+       var membersAdded = message.membersAdded.map(function (x) {
+                var isSelf = x.id == message.address.bot.id;
+                return (isSelf ? message.address.bot.name : x.name) || ' ' + '(Id=' + x.id + ' )'
+            }).join(', ');
+
+       bot.send(new botbuilder.Message()
+            .address(message.address)
+            .text('Bienvenue dans le salon ' + membersAdded));
+        }
+        if (message.membersRemoved[i].id === botId) {
+            // Say goodbye
+            var reply = new builder.Message()
+                    .address(message.address)
+                    .text('Goodbye' + membersRemoved);
+            bot.send(reply);
+        
+        }
+   });
 });
